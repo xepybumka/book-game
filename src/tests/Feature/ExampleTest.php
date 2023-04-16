87c52239e2db3cec1399d418418ba2b674ca65a3
mchernov <mchernov.work@gmail.com>
2023-04-16 20:10:23 +0400

    Edited namespaces for tests

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Tests\Feature;
+namespace App\Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+use App\Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
