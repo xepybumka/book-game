6fb5919254d59f06301f2fa6583c91d1e4c286a8
mchernov <mchernov.work@gmail.com>
2023-05-07 23:28:00 +0400

    Added base structure for admin.paragraph

--- src/resources/views/layouts/admin_layout.blade.php
+++ src/resources/views/layouts/admin_layout.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../../assets/img/favicon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset('assets/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ URL::asset('assets/img/favicon.png') }}">
     <title>
         Admin panel
     </title>
@@ -13,26 +13,27 @@
     <link rel="stylesheet" type="text/css"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700"/>
     <!-- Nucleo Icons -->
-    <link href="../../assets/css/nucleo-icons.css" rel="stylesheet"/>
-    <link href="../../assets/css/nucleo-svg.css" rel="stylesheet"/>
+    <link href="{{ URL::asset('assets/css/nucleo-icons.css') }}" rel="stylesheet"/>
+    <link href="{{ URL::asset('assets/css/nucleo-svg.css') }}" rel="stylesheet"/>
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- CSS Files -->
-    <link id="pagestyle" href="../../assets/css/material-dashboard.css?v=3.0.0" rel="stylesheet"/>
+    <link id="pagestyle" href="{{ URL::asset('assets/css/material-dashboard.css') }}" rel="stylesheet"/>
 </head>
 
 <body class="g-sidenav-show  bg-gray-200">
+<!-- Sidebar -->
 <aside
     class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
     id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
            aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
+        <a class="navbar-brand m-0" href="{{ route('admin') }}"
            target="_blank">
-            <img src="../../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="{{ URL::asset('assets/img/logo-ct.png') }}" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold text-white">Admin panel</span>
         </a>
     </div>
@@ -40,44 +41,84 @@
     <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link text-white " href="../../pages/dashboard.html">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="material-icons opacity-10">dashboard</i>
-                    </div>
-                    <span class="nav-link-text ms-1">First page</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white " href="../../pages/tables.html">
+                <a class="nav-link text-white " href="{{ route('paragraphs.list') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">table_view</i>
                     </div>
-                    <span class="nav-link-text ms-1">Second Page</span>
+                    <span class="nav-link-text ms-1">Параграфы</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="#!">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">table_view</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Оружие</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="#!">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">table_view</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Предметы</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="#!">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">table_view</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Противники</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="#!">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">table_view</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Заметки</span>
+                </a>
+            </li>
+        </ul>
+        <hr class="horizontal light mt-0 mb-2">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link text-white " href="#!">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">receipt_long</i>
+                    </div>
+                    <span class="nav-link-text ms-1">XML Reader</span>
                 </a>
             </li>
         </ul>
     </div>
 </aside>
+<!-- End Sidebar -->
 <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
     <!-- Navbar -->
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
          navbar-scroll="true">
         <div class="container-fluid py-1 px-3">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a>
-                    </li>
-                    <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Template</li>
-                </ol>
-                <h6 class="font-weight-bolder mb-0">Template</h6>
-            </nav>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me -5">
+                        <li class="breadcrumb-item text-sm">
+                            <a class="opacity-5 text-dark" href="{{ route('admin') }}">
+                                Admin
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">
+                            @yield('breadcrumbs')
+                        </li>
+                    </ol>
+                </nav>
         </div>
     </nav>
     <!-- End Navbar -->
     <div class="container-fluid py-4">
         <div class="row min-vh-80 h-100">
             <div class="col-12">
-
+                @yield('content')
             </div>
         </div>
     </div>
@@ -153,15 +194,14 @@
                 </div>
             </div>
             <hr class="horizontal dark my-sm-4">
-            <a class="btn btn-outline-dark w-100" href="">View documentation</a>
         </div>
     </div>
 </div>
 <!--   Core JS Files   -->
-<script src="../../assets/js/core/popper.min.js"></script>
-<script src="../../assets/js/core/bootstrap.min.js"></script>
-<script src="../../assets/js/plugins/perfect-scrollbar.min.js"></script>
-<script src="../../assets/js/plugins/smooth-scrollbar.min.js"></script>
+<script src="{{ URL::asset('assets/js/core /popper.min.js') }}"></script>
+<script src="{{ URL::asset('assets/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ URL::asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
 <script>
     var win = navigator.platform.indexOf('Win') > -1;
     if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -174,6 +214,6 @@
 <!-- Github buttons -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="../../assets/js/material-dashboard.min.js?v=3.0.0"></script>
+<script src="{{ URL::asset('assets/js/material-dashboard.min.js') }}"></script>
 </body>
 </html>
--- src/resources/views/layouts/main_layout.blade.php
+++ src/resources/views/layouts/main_layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Book game</title>
-        <link href="../../css/app.css" rel="stylesheet"/>
+        <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet"/>
     </head>
     <body>
         <header class="header">
